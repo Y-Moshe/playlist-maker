9643641ddcfe74db4ee875543bd7741c5b4303de
Y-Moshe <moshe.nehemiah@gmail.com>
2021-10-12 19:42:04 +0300

    update index exports

--- src/App/Components/index.ts
+++ src/App/Components/index.ts
@@ -1,2 +1,6 @@
-export { Header } from './Layout/Header';
-export { Footer } from './Layout/Footer';
+export { Header } from './Layout/Header/Header';
+export { Footer } from './Layout/Footer/Footer';
+export { RightColumn } from './Layout/RightColumn/RightColumn';
+export { LeftColumn }  from './Layout/LeftColumn/LeftColumn';
+export { PlayListItem } from './UI/PlayListItem/PlayListItem';
+export { VideoPlaceholder } from './UI/VideoPlaceholder/VideoPlaceholder';
