f9b07899557cc7e3194a78c818fc0a5f5f227b0b
Y-Moshe <moshe.nehemiah@gmail.com>
2021-10-12 19:45:15 +0300

    render layout components

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import { Footer, Header } from './Components';
+import { Player, Youtube } from '@vime/react';
+import { Button, Box } from '@mui/material';
+
+import { Footer, Header, LeftColumn, RightColumn } from './Components';
 
 function App() {
   return (
-    <div>
+    <div style = {{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Header />
+
+      <Box sx = {{ flexGrow: 1, width: 1, display: 'flex' }} component = "main">
+        <LeftColumn />
+
+        <Box sx = {{ flexGrow: 1 }}>
+          <Player>
+            <Youtube videoId = "XJcXjUB0yqY" />
+          </Player>
+          <Button>Randomize playlist</Button>
+          <Button>Save playlist</Button>
+        </Box>
+
+        <RightColumn />
+      </Box>
 
       <Footer />
     </div>
