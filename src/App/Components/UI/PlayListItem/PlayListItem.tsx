0c225c9cd3c931d4b47bf50fda8859ddb5aa1559
Y-Moshe <moshe.nehemiah@gmail.com>
2021-10-12 19:41:41 +0300

    UI Components

---
+++ src/App/Components/UI/PlayListItem/PlayListItem.tsx
@@ -0,0 +1,7 @@
+export function PlayListItem() {
+  return (
+    <div>
+      PlaylistItem
+    </div>
+  )
+}
---
+++ src/App/Components/UI/VideoPlaceholder/VideoPlaceholder.tsx
@@ -0,0 +1,7 @@
+export function VideoPlaceholder() {
+  return (
+    <div>
+      VideoPlaceholder
+    </div>
+  )
+}
