59e3d35675ae652ae25db59e7d6cdb9f28539584
Y-Moshe <moshe.nehemiah@gmail.com>
2021-10-19 17:33:08 +0300

    add REACT_APP_YT_BASE_URL

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,6 +3,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test';
     PUBLIC_URL: string;
+    REACT_APP_YT_BASE_URL: string;
     REACT_APP_API_KEY: string;
   }
 }
