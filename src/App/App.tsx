2cc80c700d35b8e904973bae64bc5b75ea0ae8ec
Y-Moshe <moshe.nehemiah@gmail.com>
2021-11-10 20:17:47 +0200

    update Application code

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,27 +1,123 @@
+import { useState, CSSProperties } from 'react';
 import { Player, Youtube } from '@vime/react';
 import { Button, Box } from '@mui/material';
+import { SxProps, Theme } from '@mui/system';
 
 import { Footer, Header, LeftColumn, RightColumn } from './Components';
+import { VideoPlaceholderType } from './Types';
+
+const appStyle: CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  minHeight: '100vh'
+};
+
+const mainStyle: SxProps<Theme> = {
+  flexGrow: 1,
+  width: 1,
+  display: 'flex'
+};
+
+const playerActionsStyle: SxProps<Theme> = {
+  display: 'flex',
+  justifyContent: 'space-around'
+};
+
+const playerContainerStyle: SxProps<Theme> = {
+  width: 1,
+  m: 1,
+  boxShadow: '0 0 15px grey'
+};
 
 export default function App() {
+  const [ currentPlaying, setCurrentPlaying ] = useState( '' );
+  const [ playList, setPlaylist ] = useState<VideoPlaceholderType[]>( [] );
+
+  const handlePlayVideo = ( videoId: string ) => {
+    setCurrentPlaying( '' );
+    setTimeout(() => setCurrentPlaying( videoId ), 1000);
+    console.log('Playing ' + videoId);
+  };
+
+  const handleAddToPlaylist = ( vidProps: VideoPlaceholderType ) => {
+    setPlaylist( prev => [ ...prev, vidProps ]);
+  };
+
+  const handleRemoveFromPlaylist = ( videoId: string ) => {
+    setPlaylist( prev => [ ...prev.filter( vid => vid.id !== videoId )]);
+  };
+
+  const handleRandomizePlaylist = () => {
+    const copy = playList.slice();
+
+    for ( let i = copy.length - 1; i > 0; i-- ) {
+      const j = Math.floor( Math.random() * ( i + 1 ));
+      const temp = copy[i];
+      copy[i] = { ...copy[j] };
+      copy[j] = { ...temp };
+    }
+
+    setPlaylist( copy );
+  };
+
+  const handleSaveToPlaylist = ( videoId: string ) => {
+
+  };
+
+  const handlePlayNextInPlaylist = () => {
+    const indexOfCurrent = playList.findIndex( vid => vid.id === currentPlaying );
+    if ( playList.length <= 1 ) {
+      return;
+    }
+    const indexOfNext = indexOfCurrent + 1;
+    const videoId2Play = playList[indexOfNext]?.id;
+
+    videoId2Play && handlePlayVideo( videoId2Play );
+  };
+
   return (
-    <div style = {{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+    <div style = { appStyle }>
       <Header />
 
-      <Box sx = {{ flexGrow: 1, width: 1, display: 'flex' }} component = "main">
-        <LeftColumn />
+      <Box component = "main" sx = { mainStyle }>
+        <LeftColumn
+          playList = { playList }
+          onPlayVideo     = { handlePlayVideo }
+          onAddToPlaylist = { handleAddToPlaylist }
+        />
 
         <Box sx = {{ flexGrow: 1 }}>
-          <Player>
-            <Youtube videoId = "XJcXjUB0yqY" />
-          </Player>
-          <Button>Randomize playlist</Button>
-          <Button>Save playlist</Button>
+          {
+            currentPlaying &&
+            <Box sx = { playerContainerStyle }>
+              <Player
+                controls
+                onVmPlaybackEnded = { () => handlePlayNextInPlaylist() }>
+                <Youtube videoId  = { currentPlaying } />
+              </Player>
+            </Box>
+          }
+          <Box sx = { playerActionsStyle }>
+            <Button
+              size    = "large"
+              color   = "warning"
+              variant = "outlined"
+              onClick = { handleRandomizePlaylist }>
+                Randomize playlist
+            </Button>
+            <Button
+              size    = "large"
+              color   = "success"
+              variant = "contained"
+              onClick = { () => handleSaveToPlaylist( currentPlaying ) }>
+                Save playlist
+            </Button>
+          </Box>
         </Box>
 
         <RightColumn
-          onVideoClick = { videoId => console.log( videoId ) }
-          onAddVideo   = { videoId => console.log( videoId ) }
+          onPlayVideo     = { handlePlayVideo }
+          onAddToPlaylist = { handleAddToPlaylist }
         />
       </Box>
 
