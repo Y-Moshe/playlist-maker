203cedf2859c74d067871b5620ddb003f9f0c21d
Y-Moshe <moshe.nehemiah@gmail.com>
2021-11-15 20:22:02 +0200

    add width animation to top bar

--- src/App/Components/Layout/Header/Header.tsx
+++ src/App/Components/Layout/Header/Header.tsx
@@ -1,22 +1,30 @@
+import { useSpring, a } from 'react-spring';
 import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 import { Settings } from '@mui/icons-material';
 
 interface HeaderProps {}
 
 export function Header( props: HeaderProps ) {
+  const widthAnim = useSpring({
+    from: { width: '35%' },
+    to:   { width: '100%' }
+  });
+
   return (
-    <AppBar position = "static">
-      <Toolbar>
-        <Typography
-          variant = "h6"
-          component = "div"
-          sx = {{ flexGrow: 1 }}>
-          React Playlist Maker
-        </Typography>
-        <IconButton color = "inherit">
-          <Settings />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <a.div style = { widthAnim }>
+      <AppBar position = "static">
+        <Toolbar>
+          <Typography
+            variant = "h6"
+            component = "div"
+            sx = {{ flexGrow: 1 }}>
+            React Playlist Maker
+          </Typography>
+          <IconButton color = "inherit">
+            <Settings />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </a.div>
   )
 }
